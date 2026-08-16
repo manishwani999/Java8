bd54e852323565c551170109e762547e8335ecb7
Manish Wani <manishwani03@gmail.com>
2026-08-16 22:12:23 +0530

    lambda question

---
+++ src/main/java/lambdaExpression/functionalInterfaces/StringLength.java
@@ -0,0 +1,8 @@
+package lambdaExpression.functionalInterfaces;
+
+@FunctionalInterface
+public interface StringLength {
+
+    int strLength(String str);
+
+}
---
+++ src/main/java/lambdaExpression/functionalInterfaces/Sum.java
@@ -0,0 +1,8 @@
+package lambdaExpression.functionalInterfaces;
+
+@FunctionalInterface
+public interface Sum {
+
+    public abstract int sum(int a, int b);
+
+}
